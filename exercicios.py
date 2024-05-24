12aea4770ec738cb2efd83c3bbd8bb9e7368cf02
kaynanoliveira <kaynanoliveira357@gmail.com>
2024-05-23 23:51:10 -0300

    first commit

---
+++ exercicios.py
@@ -0,0 +1,12 @@
+#Soma
+a = ['10']
+b = ['20']
+c = ['30']
+resultado = a+b+c
+print(f'r = {resultado}')
+#Multiplicação
+a = ['10']
+b = ['20']
+c = ['30']
+Resultado = a * 2 + b * 3 + c * 4 
+print(f'r = {Resultado}')
