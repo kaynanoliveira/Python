0c79a776100d5ceceb24dcdcc289a33a1d126f8d
kaynanoliveira <kaynanoliveira357@gmail.com>
2024-05-24 22:32:11 -0300

    add inicial tamplate

--- pasta.py
+++ pasta.py
@@ -2,6 +2,10 @@
 a, b = 1, 2 
 print("Antes da Troca")
 print(f"O valor das Variaveis: a={a}, b={b}")
+#Atribuição Composta
+x = 10
+x = x + 1
+print(x)
 #Primeira Troca
 temp = a
 a = b 
@@ -20,3 +24,9 @@ def func():
 x = 10
 func()
 print(x)    
+#Função Input
+nome = input('Digite seu Nome:')
+print(nome)
+#A função input() tanto exibe na tela a string “Digite seu Nome”, como permite que o valor informado pelo usuário seja armazenado na variável nome.
+
+#OBS: em Python não é possível incrementar ou decrementar uma variável com um operador unário, como o ++ ou --.
