936a0aba5d127da1a72b5feda3aa769c19c59f48
kaynanoliveira <kaynanoliveira357@gmail.com>
2024-06-06 00:31:50 -0300

    commit

--- DesenvolvimentoWeb/exercicio1.py
+++ DesenvolvimentoWeb/exercicio1.py
@@ -6,4 +6,18 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/')
+def comprimento():
+    boas_vindas = '<h1> Olá, Programadores </h1>'
+    link = '<p><a href="user/Usuario">Clique Aqui!</a></p>'
+    return boas_vindas + link
 
+@app.route('/user/')
+@app.route('/user/<nome>')
+def user(nome='Usuario'):
+    personalizar = f'<h1>Olá, {nome}!</h1>'
+    instrucao = '<p> Altere o nome no <em> endereço do browser</em> e recarregue a página.</p>'
+    return personalizar + instrucao
+
+if __name__== '__main__':
+    app.run(debug=True)
